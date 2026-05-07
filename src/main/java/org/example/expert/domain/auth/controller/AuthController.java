7a13bb47d66af9aa0f6ddfcb277997315dbdec6f
116_Lv <lbw0109@gmail.com>
2026-05-07 11:35:52 +0900

    Lv.1 feat: - WebConfig에 WebMvcConfigurer를 구현하여 Resolver 추가 - AuthController에 ArgumentResolver 정상 동작 확인용 API 작성

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -7,6 +7,9 @@ import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.service.AuthService;
+import org.example.expert.domain.common.annotation.Auth;
+import org.example.expert.domain.common.dto.AuthUser;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,15 @@ public class AuthController {
     @PostMapping("/auth/signin")
     public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
         return authService.signin(signinRequest);
+    }
+
+    //resolver 동작여부 확인용 API
+    @GetMapping("/test/auth")
+    public String testAuth(@Auth AuthUser authUser) {
+        // resolver가 잘 동작한다면 authUser가 null이 아니고 값이 들어있어야 함
+        System.out.println("로그인 유저 ID: " + authUser.getId());
+        System.out.println("로그인 유저 이메일: " + authUser.getEmail());
+
+        return "인증된 유저 ID: " + authUser.getId();
     }
 }
